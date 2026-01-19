bb31e27b9f5c1accfc8093eecb710fce5f8c39f2
Hristyaa <shhs200799@gmail.com>
2026-01-19 20:54:03 +0900

    add startapps habits, users (add mosel User)

--- config/settings.py
+++ config/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "users",
+    "habits",
 ]
 
 MIDDLEWARE = [
@@ -123,5 +125,6 @@ STATIC_URL = "static/"
 MEDIA_URL = "media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
-
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "users.User"
